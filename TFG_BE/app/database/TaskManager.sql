fbb1f3ca2ff64c493ced0c2af527f8ca3aa0d1c7
ManishaStudent26 <20063240@mydbs.ie>
2026-05-04 16:12:59 +0100

    fetch: update and create of tables in SQL file before MySQL testing.

--- TFG_BE/app/database/TaskManager.sql
+++ TFG_BE/app/database/TaskManager.sql
@@ -10,19 +10,40 @@ email VARCHAR(100) NOT NULL UNIQUE,
 password_hash(255) NOT NULL,
 name VARCHAR(100) NOT NULL,
 phonenumber VARCHAR(20) NOT NULL,
-role ENUM('Volunteer', 'Admin', 'Event Manager') DEFAULT 'Volunteer')
-
-CREATE TABLE IF NOT EXISTS Availability(
-    log_id INT AUTO_INCREMENT PRIMARY KEY,
-    uid INT FOREIGN KEY NULL,
-    log_year YEAR NOT NULL
-    week_number TINYINT NOT NULL,
-    available_hours DECIMAL(4,2)
-
+role ENUM('Volunteer', 'Admin', 'Event Manager') DEFAULT 'Volunteer',
+INDEX idx_email (email)
 )
 
-CREATE TABLE IF NOT EXISTS Projects()
+CREATE TABLE IF NOT EXISTS Availability(
+    timelog_id INT AUTO_INCREMENT PRIMARY KEY,
+    uid INT FOREIGN KEY NOT NULL,
+    timelog_year YEAR NOT NULL
+    week_number TINYINT NOT NULL,
+    available_hours DECIMAL(4,2)
+)
 
-CREATE TABLE IF NOT EXISTS Tasks()
+CREATE TABLE IF NOT EXISTS Projects(
+    project_id INT AUTO_INCREMENT PRIMARY KEY,
+    uid INT FOREIGN KEY NOT NULL,
+    project_name VARCHAR(255) NOT NULL,
+    project_start DATE NO NULL,
+    project_end DATE
+)
 
-CREATE TABLE IF NOT EXISTS ProjectUsers()
+CREATE TABLE IF NOT EXISTS Tasks(
+    task_id INT AUTO_INCREMENT PRIMARY KEY,
+    task_nameVARCHAR(255) NOT NULL,
+    owner_id FOREIGN KEY DEFAULT NULL,
+    project_id INT FOREIGN KEY NOT NULL,
+    start_date DATE DEFAULT NULL,
+    target_date DATE DEFAULT NULL,
+    task_priority ENUM('LOW', 'MED', 'HIGH'),
+    weight DECIMAL(4,2),
+    progress DECIMAL(5,2),
+    task_status ENUM('Planned', 'Ongoing', 'Completed'))
+
+CREATE TABLE IF NOT EXISTS ProjectUsers(
+    owner_id INT AUTO_INCREMENT PRIMARY KEY,
+    project_id FOREIGN KEY NOT NULL,
+    uid FOREIGN KEY NOT NULL
+)
