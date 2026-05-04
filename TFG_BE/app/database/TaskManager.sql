9dfa5b7b9ad5081836f98725349c51911d9d65a6
ManishaStudent26 <20063240@mydbs.ie>
2026-05-04 15:32:01 +0100

    fetch: Update to DB: CREATION OF DB and USER TABLE based on previous made mySQL DB for Web and Cloud class, in class example and temporary UML design. Gemini input on how to create password in MySQL because former project used firebase.

---
+++ TFG_BE/app/database/TaskManager.sql
@@ -0,0 +1,26 @@
+/*Database creation*/
+CREATE DATABASE IF NOT EXISTS TaskManager_DB
+CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
+USE TaskManager_DB;
+
+CREATE TABLE IF NOT EXISTS Users(
+uid INT AUTO_INCREMENT PRIMARY KEY,
+email VARCHAR(100) NOT NULL UNIQUE,
+password_hash(255) NOT NULL,
+name VARCHAR(100) NOT NULL,
+phonenumber VARCHAR(20) NOT NULL,
+role ENUM('Volunteer', 'Admin', 'Event Manager') DEFAULT 'Volunteer')
+
+CREATE TABLE IF NOT EXISTS Availability(
+    log_id INT AUTO_INCREMENT PRIMARY KEY,
+    uid INT FOREIGN KEY,
+    week 
+
+)
+
+CREATE TABLE IF NOT EXISTS Projects()
+
+CREATE TABLE IF NOT EXISTS Tasks()
+
+CREATE TABLE IF NOT EXISTS ProjectUsers()
