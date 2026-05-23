f5f70e0663b08f049dfd75101750c2f6ef7810b5
ManishaStudent26 <20063240@mydbs.ie>
2026-05-23 18:00:28 +0100

    feat: connect db.py to the database. defined dotenv based on past CA adjusted connection from js to py with help from gemini. Added a few model files. Yet to be defined.

---
+++ TFG_BE/app/config/db.py
@@ -0,0 +1,17 @@
+import os
+import mysql.connector
+from mysql.connector import Error
+from dotenv import load_dotenv
+
+load_dotenv()
+
+db_pool = mysql.connector.pooling.MySQLConnectionPool(
+    pool_name="taskmanager_pool",
+    pool_size= 10,
+    host=os.getenv('DB_HOST'),
+    user=os.getenv('DB_USER'),
+    password=os.getenv('DB_PASSWORD'),
+    database=os.getenv('DB_NAME'))
+
+def get_db_connection():
+    return db_pool.get_connection()
