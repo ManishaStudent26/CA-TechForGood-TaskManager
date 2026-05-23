7edefc77ce98d6470e5cce82bf2363af908f8f56
ManishaStudent26 <20063240@mydbs.ie>
2026-05-23 22:40:35 +0100

    feat: adjust encapsulate password hash + getUserbyEmail function written. Rest commented out. Untested.

--- TFG_BE/app/models/user_model.py
+++ TFG_BE/app/models/user_model.py
@@ -5,14 +5,21 @@ class User:
     def __init__(self, uid, email, password_hash, name, role):
         self.uid = uid
         self.email = email
-        self.password_hash = password_hash
+        self._password_hash = password_hash
         self.name = name
         self.role = role
 
         @classmethod
-        def getUserbyEmail(cls):
+        def getUserbyEmail(cls, email):
+            connection=connection.cursor(dictionary=True)
+            cursor=execute("SELECT FROM USERS WHERE email=%s", (email,))
+        if row:
+            return cls(row['uid'], row['email'], row['password_hash'], row['name'], row['role'])
+        return None
 
-        def getUserByID(cls):
+       """def getUserByID(cls):
         
-        def createUser(cls):
+        def createUser(cls):"""
+    
+
         
