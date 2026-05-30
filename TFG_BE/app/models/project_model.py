c4c8f18f8ebaebbf2c7d59ff27b3859db264934a
ManishaStudent26 <20063240@mydbs.ie>
2026-05-30 12:12:21 +0100

    feat: added additional OpenTask to project class

--- TFG_BE/app/models/project_model.py
+++ TFG_BE/app/models/project_model.py
@@ -1,6 +1,6 @@
 from config.database import get_db_connection
 class Project:
-   def __init__(self, pid, project_owner, project_name, project_start, project_end, project_status):
+   def __init__(self, pid, project_owner, project_name, project_start, project_end, project_status, opentasks):
         self.pid = pid
         self.project_owner = project_owner
         self.project_name = project_name
@@ -8,6 +8,7 @@ class Project:
         self.project_start = project_start
         self.project_end = project_end
         self.project_status= project_status
+        self.opentasks= opentasks
         @property
         def status(self):
             today = date.today()
@@ -17,6 +18,16 @@ class Project:
                 return "In Progress"
             else:
                 return "Completed"
+def to_dict(self):
+    return {
+        "pid": self.pid,
+        "project_owner": self.project_owner,
+        "project_name": self.project_name,
+        "project_start": self.project_start.isoformat() if self.project_start else None,
+        "project_end": self.project_end.isoformat() if self.project_end else None,
+        "project_status": self.project_status, # Evaluates the property dynamically
+        "opentasks": self.opentasks
+        }
 
 
 @classmethod
@@ -24,7 +35,7 @@ def getProjectbyManager(cls,user_uid):
     connection= get_db_connection()
     cursor=connection.cursor(dictionary=True)
     try:
-        query = "SELECT * FROM Projects WHERE project_owner = %s"
+        query = "SELECT  a.*, b.COUNT(Task_ID) AS opentasks FROM Projects a JOIN WHERE a.project_id=b.project_id AND a.project_owner = %s AND b.status NOT IN 'Completed' "
         cursor.execute(query, (user_uid,))
         rows = cursor.fetchall()
         projects = []
@@ -34,6 +45,7 @@ def getProjectbyManager(cls,user_uid):
                 project_name=row['project_name'],
                 project_start=row['project_start'],
                 project_end=row['project_end'],
+                opentasks=row('opentasks')
             ))
         return projects
     finally:
