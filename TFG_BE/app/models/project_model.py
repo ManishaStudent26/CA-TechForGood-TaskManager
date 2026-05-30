77359d3d38093ca29fb2ee7c6d90dc65a11ee90b
ManishaStudent26 <20063240@mydbs.ie>
2026-05-30 15:23:56 +0100

    fix: Testing getProjectbyManager with insomnia. fixed indent issue, forgot import date function on top and naming mismatches.

--- TFG_BE/app/models/project_model.py
+++ TFG_BE/app/models/project_model.py
@@ -1,72 +1,75 @@
-from config.database import get_db_connection
+from config.db import get_db_connection
+from datetime import date
+
 class Project:
-   def __init__(self, pid, project_owner, project_name, project_start, project_end, project_status, opentasks):
+    def __init__(self, pid, project_owner, project_name, project_start, project_end, project_status, opentasks):
         self.pid = pid
         self.project_owner = project_owner
         self.project_name = project_name
-        self.project_start = project_name
         self.project_start = project_start
         self.project_end = project_end
         self.project_status= project_status
         self.opentasks= opentasks
-        @property
-        def status(self):
-            today = date.today()
-            if today < self.start_date:
-                return "Upcoming"
-            elif self.start_date <= today <= self.end_date:
-                return "In Progress"
-            else:
-                return "Completed"
-def to_dict(self):
-    return {
-        "pid": self.pid,
-        "project_owner": self.project_owner,
-        "project_name": self.project_name,
-        "project_start": self.project_start.isoformat() if self.project_start else None,
-        "project_end": self.project_end.isoformat() if self.project_end else None,
-        "project_status": self.project_status, # Evaluates the property dynamically
-        "opentasks": self.opentasks
-        }
+    
+    @property
+    def status(self):
+        today = date.today()
+        if today < self.project_start:
+            return "Upcoming"
+        elif self.start_date <= today <= self.project_end:
+            return "In Progress"
+        else:
+            return "Completed"
 
+    def to_dict(self):
+        return {
+            "pid": self.pid,
+            "project_owner": self.project_owner,
+            "project_name": self.project_name,
+            "project_start": self.project_start.isoformat() if self.project_start else None,
+            "project_end": self.project_end.isoformat() if self.project_end else None,
+            "project_status": self.project_status, # Evaluates the property dynamically
+            "opentasks": self.opentasks
+            }
 
-@classmethod
-def getProjectbyManager(cls,user_uid):
-    connection= get_db_connection()
-    cursor=connection.cursor(dictionary=True)
-    try:
+    @classmethod
+    def getProjectbyManager(cls,user_uid):
+        connection= get_db_connection()
+        cursor=connection.cursor(dictionary=True)
+        try:
             query = """
-                SELECT 
-                    p.project_id, 
-                    p.project_owner, 
-                    p.project_name, 
-                    p.project_start, 
-                    p.project_end, 
-                    COUNT(t.Task_ID) AS opentasks 
+            SELECT 
+                p.project_id, 
+                p.project_owner, 
+                p.project_name, 
+                p.project_start, 
+                p.project_end, 
+                COUNT(t.Task_ID) AS opentasks 
                 FROM Projects p 
-                LEFT JOIN Tasks t ON p.project_id = t.project_id AND t.status != 'Completed'
+                LEFT JOIN Tasks t ON p.project_id = t.project_id AND t.task_status != 'Completed'
                 WHERE p.project_owner = %s 
                 GROUP BY p.project_id
-            """
+                """
             cursor.execute(query, (user_uid,))
             rows = cursor.fetchall()
             
             projects = []
             for row in rows:
                 projects.append(cls(
-                    pid=row['project_id'],
-                    project_owner=row['project_owner'],
-                    project_name=row['project_name'],
-                    project_start=row['project_start'],
-                    project_end=row['project_end'],
-                    opentasks=row['opentasks']
+                pid=row['project_id'],
+                project_owner=row['project_owner'],
+                project_name=row['project_name'],
+                project_start=row['project_start'],
+                project_end=row['project_end'],
+                project_status= None,
+                opentasks=row['opentasks']
                 ))
-            return projects
-    finally:
-        cursor.close()
-        connection.close()
-@classmethod
+                return projects
+        finally:
+            cursor.close()
+            connection.close()
+"""@classmethod
 def createProject:
 
 @classmethod
-def deleteProject:
+def deleteProject:"""
