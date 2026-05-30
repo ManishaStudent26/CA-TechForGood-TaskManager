29a6833cff4d20c0f545af8e20f567ba98069956
ManishaStudent26 <20063240@mydbs.ie>
2026-05-30 18:41:55 +0100

    Feat: Added CORS to connect frontend to backend.

--- TFG_BE/app/backend.py
+++ TFG_BE/app/backend.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from flask_cors import CORS
 from routes.user_routes import user_bp 
 from routes.auth_routes import auth_bp
 from routes.project_routes import projects_bp
 
 app = Flask(__name__)
+CORS(app)
 
 app.register_blueprint(user_bp)
 app.register_blueprint(auth_bp)
