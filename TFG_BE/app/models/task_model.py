a0510eaaa7d83f8ffaa502b66eef6be2007cce28
ManishaStudent26 <20063240@mydbs.ie>
2026-05-31 20:02:52 +0100

    feat: add property to task class(to be fixed)

--- TFG_BE/app/models/task_model.py
+++ TFG_BE/app/models/task_model.py
@@ -1,11 +1,11 @@
 from config.db import get_db_connection
 from datetime import date
 class Task:
-    def __init__(self, taskid, taskname, taskowner, project_name, startdate, targetdate, taskpri, weight, progress, status, overdue):
+    def __init__(self, taskid, taskname, taskowner, projectname, startdate, targetdate, taskpri, weight, progress, status, overdue):
      self.taskid=taskid
      self.taskname=taskname
      self.taskowner=taskowner
-     self.pid=pid
+     self.projectname=projectname
      self.startdate=startdate
      self.targetdate=targetdate
      self.taskpri=taskpri
@@ -14,9 +14,19 @@ class Task:
      self.status=status
      self.overdue=overdue
 
+    @property
+    def overdue(self):
+        today = date.today()
+        if self.status==('Completed'):
+          return  False
+        elif today >= self.targetdate:
+          return True
+        else:
+          return False
 
-"""@classmethod
-def getTaskbyProject
+       
+    @classmethod
+    def getTaskbyProject:
 
 
 def createTask
