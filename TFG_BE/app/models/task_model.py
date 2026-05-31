e5d50a256249873fc547ad151eb4f499ee8678d6
ManishaStudent26 <20063240@mydbs.ie>
2026-05-31 16:29:07 +0100

    feat: defined class tasks in task_model.py

--- TFG_BE/app/models/task_model.py
+++ TFG_BE/app/models/task_model.py
@@ -1,11 +1,29 @@
-from config.database import get_db_connection
-class Task
+from config.db import get_db_connection
+from datetime import date
+class Task:
+    def __init__(self, taskid, taskname, taskowner, pid, startdate, targetdate, taskpri, weight, progress, status, overdue):
+     self.taskid=taskid
+     self.taskname=taskname
+     self.taskowner=taskowner
+     self.pid=pid
+     self.startdate=startdate
+     self.targetdate=targetdate
+     self.taskpri=taskpri
+     self.weight=weight
+     self.progress=progress
+     self.status=status
+     self.overdue=overdue
 
+
+"""@classmethod
 def getTaskbyProject
 
-def getTaskbyOwner
+
+def createTask
+
+def getTaskbyContributor
 
 def setTask
 
-def delTask
+def delTask"""
 
