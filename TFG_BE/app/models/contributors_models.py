ca65cc75bc9e87e1fa4699ed936259cb43fe339f
ManishaStudent26 <20063240@mydbs.ie>
2026-05-31 19:23:22 +0100

    fix: missing pid value in class definition. feat: written get function for Contributor(not tested)

--- TFG_BE/app/models/contributors_models.py
+++ TFG_BE/app/models/contributors_models.py
@@ -3,14 +3,42 @@ from config.db import get_db_connection
 class Contributor:
     def __init__(self, cid, pid, uid, name):
         self.cid=cid
-        self.pid=uid
+        self.pid=pid
+        self.uid=uid
         self.name=name
 
-@classmethod
-def getContributorbyProject():
+    @classmethod
+    def getContributorbyProject(cls,pid):
+        connection= get_db_connection()
+        cursor=connection.cursor(dictionary=True)
+        try:
+            query = """
+            SELECT 
+                p.contributor_id,
+                p.project_id,
+                p.uid,
+                u.name
+                FROM ProjectsUsers p 
+                LEFT JOIN Users u ON p.uid= u.uid
+                WHERE project_id=%s
+                """
+            cursor.execute(query, (pid,))
+            rows = cursor.fetchall()
+            
+            projects = []
+            for row in rows:
+                projects.append(cls(
+                pid=row['project_id'],
+                uid=row['uid'],
+                name=row['name'],
+                ))
+                return projects
+        finally:
+            cursor.close()
+            connection.close()
 
-@classmethod
-def addContributortoProject():
+    @classmethod
+    def addContributortoProject():
 
-@classmethod
-def removeContributorfromProject():
+    @classmethod
+    def removeContributorfromProject():
