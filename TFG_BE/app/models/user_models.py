8f413195fbf71c74c30cda19a563d68e27a1c267
ManishaStudent26 <20063240@mydbs.ie>
2026-06-01 19:52:05 +0100

    feat: created EditUser for admin

--- TFG_BE/app/models/user_models.py
+++ TFG_BE/app/models/user_models.py
@@ -18,5 +18,21 @@ class User:
         if row:
             return cls(row['uid'], row['email'], row['password_hash'], row['name'], row['role'])
         return None
+    @classmethod
+    def EditUser(uid, email, name, role):
+        connection= get_db_connection()
+        cursor=connection.cursor
+        try:
+            query = """
+            UPDATE Users
+            SET email=%s, name=%s, role=%s
+            WHERE uid=%s"""
+            cursor.execute(query, email, name, role, uid)
+            connection.commit()
+            return cursor.rowcount > 0 
+        finally:
+            cursor.close()
+            connection.close()
+    
 
         
