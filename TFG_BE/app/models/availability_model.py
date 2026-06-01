6206d1aa53f82641a64b98cbf3fe3e3a8674e7ea
ManishaStudent26 <20063240@mydbs.ie>
2026-06-01 21:37:42 +0100

    Feat: setAvailability function. Untested.

--- TFG_BE/app/models/availability_model.py
+++ TFG_BE/app/models/availability_model.py
@@ -7,4 +7,11 @@ class Availability:
         self.hours=hours
 
 @classmethod    
-def getAvailability():
+def getAvailability(uid):
+    connection=get_db_connection
+    cursor=connection.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM Availability WHERE email=%s", (uid,))
+    rows =cursor.fetchall()
+    for row in rows:
+        return cls(row['uid'], row['ema'], row['password_hash'], row['name'], row['role'])
+    return None
