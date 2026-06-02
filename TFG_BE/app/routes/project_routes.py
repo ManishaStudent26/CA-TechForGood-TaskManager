a6cdccbc6c1c6aef744d240d4fb443744dcdd4f6
ManishaStudent26 <20063240@mydbs.ie>
2026-06-02 19:21:39 +0100

    fix: renamed session_model ro middleware.py, and moved to a new folder called utils because pylance did not recognise the function(based on gemini suggestion)

--- TFG_BE/app/routes/project_routes.py
+++ TFG_BE/app/routes/project_routes.py
@@ -1,38 +1,10 @@
 import jwt
 from flask import Blueprint, jsonify, request, g
-from functools import wraps
-
 from config.settings import Config
-from models.project_model import Project  
+from models.project_model import Project
+from utils.middleware import token_required
 
 projects_bp = Blueprint('projects', __name__)
-
-def token_required(f):
-    @wraps(f)
-    def decorated(*args, **kwargs):
-        auth_header = request.headers.get('Authorization')
-        
-        if not auth_header or not auth_header.startswith('Bearer '):
-            return jsonify({"error": "Access denied. Missing or malformed token."}), 401
-        
-        token = auth_header.split(" ")[1]
-        
-        try:
-            decoded_payload = jwt.decode(token, Config.JWT_SECRET, algorithms=["HS256"])
-            logged_in_uid = decoded_payload.get('user_id')
-            
-            if not logged_in_uid:
-                return jsonify({"error": "Invalid token payload: User ID missing."}), 401
-            
-            g.user_id = logged_in_uid
-            
-        except jwt.ExpiredSignatureError:
-            return jsonify({"error": "Your login session has expired. Please log in again."}), 401
-        except jwt.InvalidTokenError:
-            return jsonify({"error": "Invalid authentication token."}), 401
-            
-        return f(*args, **kwargs)
-    return decorated
 
 @projects_bp.route('/api/projects', methods=['GET'])
 @token_required
