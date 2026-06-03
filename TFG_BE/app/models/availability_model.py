40628920c15e6558e9bbaff6b33fc2e0dd9f8d2a
ManishaStudent26 <20063240@mydbs.ie>
2026-06-03 02:14:16 +0100

    fix: forgot to add year availability class and to all functions.
    fix: misnamed availability_routes.py. had two availability models.

--- TFG_BE/app/models/availability_model.py
+++ TFG_BE/app/models/availability_model.py
@@ -1,8 +1,9 @@
 from config.db import get_db_connection
 
 class Availability:
-    def __init__(self,uid,week,hours):
+    def __init__(self,uid,year,week,hours):
         self.uid=uid
+        self.year=year
         self.week=week
         self.hours=hours
 
@@ -10,35 +11,36 @@ class Availability:
 def getAvailability(cls,uid):
     connection=get_db_connection
     cursor=connection.cursor(dictionary=True)
-    cursor.execute("SELECT * FROM Availability WHERE email=%s", (uid,))
+    cursor.execute("SELECT * FROM Availability WHERE uid=%s", (uid,))
     rows =cursor.fetchall()
     for row in rows:
-        return cls(row['uid'], row['week'], row['hour'])
+        return cls(row['uid'], row['year'],row['week'], row['hour'])
     return None
 
 @classmethod
-def setAvailability(uid, week, hours):
+def setAvailability(uid, week, year, hours):
     connection=get_db_connection
     cursor=connection.cursor
     try:
-        query="""INSERT INTO Availability (uid, week, hours),
-        VALUES (%s, %s, %s)"""
-        cursor.execute(query, (uid, week, hours))
+        query="""INSERT INTO Availability (uid, week, year, hours),
+        VALUES (%s, %s, %s, %s)"""
+        cursor.execute(query, (uid, week, year,hours))
         connection.commit()
     finally:
         cursor.close()
         connection.close()
 
 @classmethod
-def updateAvailability(uid,week, hours):
+def updateAvailability(uid,week, year, hours):
     connection=get_db_connection
     cursor=connection.cursor
     try:
         query="""UPDATE Availability
         SET hours=%s
         WHERE uid=%s
-        AND week=%s"""
-        cursor.execute(query,hours, uid, week)
+        AND week=%s
+        AND year=%s"""
+        cursor.execute(query,hours, uid, week, year)
         connection.commit
     finally:
         cursor.close()
--- TFG_BE/app/routes/availability_models.py
+++
@@ -1,3 +0,0 @@
-from flask import Blueprint, jsonify, request, g
-from models.availability_model import Availability
-from utils.middleware import token_required
