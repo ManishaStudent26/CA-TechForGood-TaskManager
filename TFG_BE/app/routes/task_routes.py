586eb8a669532e0bc546cc84530888507a97132c
ManishaStudent26 <20063240@mydbs.ie>
2026-06-03 18:45:14 +0100

    feat: started on task routes. both to be fixed & finished

--- TFG_BE/app/routes/task_routes.py
+++ TFG_BE/app/routes/task_routes.py
@@ -1,6 +1,28 @@
-from flask import Blueprint, jsonify, g
+from flask import Blueprint, jsonify, g, request
 from models.task_model import Task
 from utils.middleware import token_required
 
 task_bp=Blueprint(Task,__name__)
 
+@task_bp.route ('/api/projecttasks', methods=['GET'])
+@token_required
+def  getprojecttasks():
+    project_tasks = Task.getTaskbyProject(g.pid)
+    serialized_tasks=[task.to_dic() for tasks in project_tasks]
+    return jsonify(serialized_tasks), 200
+
+@task_bp.route('api/tasks', method=['POST'])
+@token_required
+def createprojecttask():
+    data=request.get_json()
+    data.get('taskname')
+    data.get('pid')
+    data.get('startdate')
+    data.get('targetdate')
+    data.get('taskpri')
+    data.get('weight')
+    data.get('status')
+try:
+    new_task=Task.createTask(
+        
+    )
