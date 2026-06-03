37d3f71202c32646063a634aabd596bfb26394b3
ManishaStudent26 <20063240@mydbs.ie>
2026-06-03 16:06:44 +0100

    fix: missed parenthesis

--- TFG_BE/app/models/task_model.py
+++ TFG_BE/app/models/task_model.py
@@ -260,7 +260,7 @@ class Task:
         raise jsonify({"error: volunteer is not available"})
       else:
         connection=get_db_connection()
-        cursor=connection.cursor
+        cursor=connection.cursor()
         try:
          query="""
           UPDATE Tasks
