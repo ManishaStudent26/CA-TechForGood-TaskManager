d935d07b9a1c7597431a90f1a5fe206128bfe31c
ManishaStudent26 <20063240@mydbs.ie>
2026-06-03 10:00:00 +0100

    feat: create post route for availability. Requested support from gemini on how to structure the except only with generic error instead of the example in project.

--- TFG_BE/app/routes/availability_routes.py
+++ TFG_BE/app/routes/availability_routes.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, jsonify, request, g
 from models.availability_model import Availability
 from utils.middleware import token_required
-from utils.errorHandling import ValidationError, ResourceNotFoundError
+from utils.errorHandling import ValidationError, ResourceNotFoundError, FailedToCreate
 
 availability_bp=Blueprint('availability',__name__)
 
@@ -13,3 +13,26 @@ def getAvailabilitybyUID():
     get_availability = Availability.getAvailability(uid)
     volunteer_availability=[availabilty.to_dict() for availability in get_availability]
     return jsonify(volunteer_availability),200
+
+@availability_bp.route('/api/availabiliy/<int:uid>', method=['POST'])
+@token_required
+def create_new_project():
+    data = request.get_json
+    uid=data.get('uid')
+    year=data.get('year')
+    week=data.get('week')
+    hours=data.get('hours')
+
+    if not uid or not year or not week or not hours:
+        raise ValidationError()
+    try:
+        new_availability= Availability.setAvailability(
+            uid=uid,
+            year=year
+            week=week
+            hours=hours
+        )
+        return jsonify(new_availability.to_dict()),201
+    except: ValueError
+    raise FailedToCreate()
+@
