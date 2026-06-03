4c383d010271aea64f7fe7e798fc9225a659d365
ManishaStudent26 <20063240@mydbs.ie>
2026-06-03 20:42:31 +0100

    feat: finalized two routes get and post

--- TFG_BE/app/routes/task_routes.py
+++ TFG_BE/app/routes/task_routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, jsonify, g, request
 from models.task_model import Task
 from utils.middleware import token_required
+from utils.errorHandling import ValidationError
 
 task_bp=Blueprint(Task,__name__)
 
@@ -8,21 +9,32 @@ task_bp=Blueprint(Task,__name__)
 @token_required
 def  getprojecttasks():
     project_tasks = Task.getTaskbyProject(g.pid)
-    serialized_tasks=[task.to_dic() for tasks in project_tasks]
+    serialized_tasks=[project_task.to_dic() for project_task in project_tasks]
     return jsonify(serialized_tasks), 200
 
 @task_bp.route('api/tasks', method=['POST'])
 @token_required
 def createprojecttask():
     data=request.get_json()
-    data.get('taskname')
-    data.get('pid')
-    data.get('startdate')
-    data.get('targetdate')
-    data.get('taskpri')
-    data.get('weight')
-    data.get('status')
-try:
-    new_task=Task.createTask(
-        
-    )
+    taskname=data.get('taskname')
+    pid=data.get('pid')
+    startdate=data.get('startdate')
+    targetdate=data.get('targetdate')
+    taskpri=data.get('taskpri')
+    weight=data.get('weight')
+    status=data.get('status')
+
+    if not taskname or not startdate or not taskpri or not status
+        raise ValidationError({})
+    try:
+        new_task=Task.createTask(
+            taskname=taskname,
+            pid=g.pid,
+            startdate=startdate,
+            targetdate=targetdate,
+            taskpri=taskpri,
+            weight=weight
+            status=status)
+        return jsonify(new_task.to_dict()), 201
+    except Exception as e:
+        return jsonify({"error": "Failed to create task", "details": str(e)}), 500
