96108ceca44106b17a4f6b1d00392b367fa8767f
ManishaStudent26 <20063240@mydbs.ie>
2026-06-03 17:28:07 +0100

    fix: indents to move class method into the class, adding missing parenthesis and removing accidental additional comma.

--- TFG_BE/app/models/availability_model.py
+++ TFG_BE/app/models/availability_model.py
@@ -16,49 +16,49 @@ class Availability:
         
 
 
-@classmethod    
-def getAvailability(cls,uid):
-    connection=get_db_connection()
-    cursor=connection.cursor(dictionary=True)
-    try:
-        cursor.execute("SELECT * FROM Availability WHERE uid=%s", (uid,))
-        rows=cursor.fetchall()
-        availabilities = []
-        for row in rows: availabilities.append({
+    @classmethod    
+    def getAvailability(cls,uid):
+        connection=get_db_connection()
+        cursor=connection.cursor(dictionary=True)
+        try:
+            cursor.execute("SELECT * FROM Availability WHERE uid=%s", (uid,))
+            rows=cursor.fetchall()
+            availabilities = []
+            for row in rows: availabilities.append({
             'uid':row['uid'],
             'year':row['year'],
             'week':row['week'],
             'hours':row['hours']})
-        return availabilities
-    finally:
-        cursor.close()
-        connection.close()
+            return availabilities
+        finally:
+            cursor.close()
+            connection.close()
 
-@classmethod
-def setAvailability(cls,uid, week, year, hours):
-    connection=get_db_connection()
-    cursor=connection.cursor
-    try:
-        query="""INSERT INTO Availability (uid, week, year, hours),
-        VALUES (%s, %s, %s, %s)"""
-        cursor.execute(query, (uid, week, year,hours))
-        connection.commit()
-    finally:
-        cursor.close()
-        connection.close()
+    @classmethod
+    def setAvailability(cls,uid, week, year, hours):
+        connection=get_db_connection()
+        cursor=connection.cursor
+        try:
+            query="""INSERT INTO Availability (uid, week, year, hours)
+            VALUES (%s, %s, %s, %s)"""
+            cursor.execute(query, (uid, week, year,hours))
+            connection.commit()
+        finally:
+            cursor.close()
+            connection.close()
 
-@classmethod
-def updateAvailability(cls,uid,week, year, hours):
-    connection=get_db_connection()
-    cursor=connection.cursor
-    try:
-        query="""UPDATE Availability
-        SET hours=%s
-        WHERE uid=%s
-        AND week=%s
-        AND year=%s"""
-        cursor.execute(query,(hours, uid, week, year))
-        connection.commit()
-    finally:
-        cursor.close()
-        connection.close()
+    @classmethod
+    def updateAvailability(cls,uid,week, year, hours):
+        connection=get_db_connection()
+        cursor=connection.cursor()
+        try:
+            query="""UPDATE Availability
+            SET hours=%s
+            WHERE uid=%s
+            AND week=%s
+            AND year=%s"""
+            cursor.execute(query,(hours, uid, week, year))
+            connection.commit()
+        finally:
+            cursor.close()
+            connection.close()
