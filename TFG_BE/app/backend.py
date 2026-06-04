ba7cf118052e68315f25d5cb066e1c1baccde614
ManishaStudent26 <20063240@mydbs.ie>
2026-06-04 14:03:06 +0100

    feat: added routes to backend.py

--- TFG_BE/app/backend.py
+++ TFG_BE/app/backend.py
@@ -3,6 +3,9 @@ from flask_cors import CORS
 from routes.user_routes import user_bp 
 from routes.auth_routes import auth_bp
 from routes.project_routes import projects_bp
+from routes.task_routes import task_bp
+from routes.availability_routes import availability_bp
+from routes.contributors_routes import contributors_bp
 
 app = Flask(__name__)
 CORS(app)
@@ -10,6 +13,9 @@ CORS(app)
 app.register_blueprint(user_bp)
 app.register_blueprint(auth_bp)
 app.register_blueprint(projects_bp)
+app.register_blueprint(task_bp)
+app.register_blueprint(availability_bp)
+app.register_blueprint(contributors_bp)
 
 
 if __name__ == '__main__':
