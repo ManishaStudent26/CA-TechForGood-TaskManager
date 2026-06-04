805dbfb7e76238573b3bbf67b86e13a3a4692e35
ManishaStudent26 <20063240@mydbs.ie>
2026-06-04 15:36:31 +0100

    fix: added gemini suggested path. Still needs to be tested.

--- TFG_BE/app/routes/task_routes.py
+++ TFG_BE/app/routes/task_routes.py
@@ -1,7 +1,9 @@
 import os
 import sys
 current_dir = os.path.dirname(os.path.abspath(__file__))
-sys.path.append(os.path.dirname(current_dir))
+parent_dir = os.path.dirname(current_dir)
+if parent_dir not in sys.path:
+    sys.path.insert(0, parent_dir)
 from flask import Blueprint, jsonify, g, request
 from models.task_model import Task
 from utils.middleware import token_required
